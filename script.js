ca6747dfc9191742440c445ecccf95510ab2ac41
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-10 23:32:36 -0700

    Display updated and stores values.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function switchMode(e) {
+
+}
+
+function calculate(mode) {
+  if (mode === null) return;
+  switch (mode) {
+    case "add":
+      display.value = add(first, second);
+      break;
+    case "subtract":
+      display.value = subtract(first, second);
+      break;
+    case "multiply":
+      display.value = multiply(first, second);
+      break;
+    case "divide":
+      display.value = divide(first, second);
+      break;
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -15,18 +37,34 @@ function divide(a, b) {
 }
 
 function number(e) {
-  console.log("hi");
   display.value === "0" ?
     display.value = e.currentTarget.id :
     display.value += e.currentTarget.id;
-
+  if (mode === null) {
+    first === null ?
+      first = e.currentTarget.id :
+      first += e.currentTarget.id;
+  } else if (mode !== null) {
+    second === null ?
+      second = e.currentTarget.id :
+      second += e.currentTarget.id;
+  }
 }
 
-let buttons = [...document.querySelectorAll("button")];
+let main = [...document.querySelectorAll(".main > button")];
+let operators = [...document.querySelectorAll(".operators > button")];
 let display = document.querySelector(".display");
+let mode = null;
+let first = null;
+let second = null;
 
-for (let i = 0; i < buttons.length; i++) {
-  isNaN(buttons[i].id) ?
-    buttons[i].addEventListener("click", buttons[i].id): // button id = function
-    buttons[i].addEventListener("click", number);
+for (let i = 0; i < main.length; i++) {
+  if (!isNaN(main[i].id)) main[i].addEventListener("click", number);
 }
+
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", switchMode); // button id = function
+}
+
+document.querySelector("#dot").addEventListener("click", number);
+document.querySelector("#equals").addEventListener("click", calculate);
