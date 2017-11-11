add81345cd78589f314303a4351182f36f644646
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-10 21:47:09 -0700

    Event listeners added to buttons.

--- script.js
+++ script.js
@@ -14,4 +14,19 @@ function divide(a, b) {
   return a / b;
 }
 
+function number(e) {
+  console.log("hi");
+  display.value === "0" ?
+    display.value = e.currentTarget.id :
+    display.value += e.currentTarget.id;
+
+}
+
+let buttons = [...document.querySelectorAll("button")];
+let display = document.querySelector(".display");
+
+for (let i = 0; i < buttons.length; i++) {
+  isNaN(buttons[i].id) ?
+    buttons[i].addEventListener("click", buttons[i].id): // button id = function
+    buttons[i].addEventListener("click", number);
 }
